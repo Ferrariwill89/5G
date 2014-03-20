556a14f2f798f832fee490fda998c568f15b62bc
Ferrariwill89 <ferrari_william89@hotmail.com>
2014-03-20 09:41:20 -0600

    Update CRecord.java

--- CRecord.java
+++ CRecord.java
@@ -6,6 +6,15 @@ private Double peso;
 private Double estatura;
 
 /** Implementar constructores **/
+public cRecord(){}
+
+public cRecord(String c, String n, String a, Double p, Double e){
+   clave = c;
+   nombre = n;
+   apellido = a;
+   peso = p;
+   estatura = e;
+}
 
 /** Implementar setters **/
 
